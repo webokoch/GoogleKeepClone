098ba2c55da79428baa7d2d64ed07c04d645e124
Stefan Koch <webokoch@gmx.de>
2022-10-10 15:12:34 +0200

    adding notes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ class App {
         this.$form = document.querySelector("#form")
         this.$noteTitle = document.querySelector("#note-title")
         this.$noteText = document.querySelector("#note-text")
+        this.$notes = document.querySelector("#notes-container");
+        this.$placeholder = document.querySelector("#placeholder");
         this.$formButtons = document.querySelector("#form-buttons")
   
       this.addEventListeners()
@@ -38,15 +40,17 @@ class App {
     }
   
     openForm() {
-      this.$form.classList.add("form-open")
-      this.$noteTitle.style.display = "block"
-      this.$formButtons.style.display = "block"
+        this.$form.classList.add("form-open")
+        this.$noteTitle.style.display = "block"
+        this.$formButtons.style.display = "block"
     }
   
     closeForm() {
-      this.$form.classList.remove("form-open")
-      this.$noteTitle.style.display = "none"
-      this.$formButtons.style.display = "none"
+        this.$form.classList.remove("form-open")
+        this.$noteTitle.style.display = "none"
+        this.$formButtons.style.display = "none"
+        this.$noteTitle.value = "";
+        this.$noteText.value = "";
     }
 
     addNote(note) {
@@ -57,8 +61,31 @@ class App {
           id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         }
         this.notes = [...this.notes, newNote]
-        console.log(this.notes)
+        this.displayNotes();
+        this.closeForm();
       }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0;
+        this.$placeholder.style.display = hasNotes ? "none" : "flex";
+
+        this.$notes.innerHTML = this.notes
+        .map(
+            note => `
+            <div style="background: ${note.color};" class="note">
+            <div class="${note.title && "note-title"}">${note.title}</div>
+            <div class="note-text">${note.text}</div>
+            <div class="toolbar-container">
+                <div class="toolbar">
+                <i class="fa-solid fa-palette"></i>
+                <i class="fa-solid fa-trash"></i>
+                </div>
+            </div>
+            </div>
+        `
+        )
+        .join("");
+    }
 
   }
   
